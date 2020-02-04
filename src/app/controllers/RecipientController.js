2964d79aa3e4cf788d3ea209452fbc0f606756e5
Matheus Neves <matheus.neves@prodigious.com>
2020-02-04 14:57:07 -0300

    validation with Yup

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,7 +1,22 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string().required(),
+      rua: Yup.string().required(),
+      numero: Yup.string().required(),
+      complemento: Yup.string(),
+      estado: Yup.string().required(),
+      cidade: Yup.string().required(),
+      cep: Yup.string().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Error in validation' });
+    }
+
     const {
       nome,
       rua,
@@ -24,6 +39,20 @@ class RecipientController {
   }
 
   async update(req, res) {
+    const schema = Yup.object().shape({
+      nome: Yup.string(),
+      rua: Yup.string(),
+      numero: Yup.string(),
+      complemento: Yup.string(),
+      estado: Yup.string(),
+      cidade: Yup.string(),
+      cep: Yup.string(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Error in validation' });
+    }
+
     const { id } = req.params;
 
     const recipient = await Recipient.findByPk(id);
