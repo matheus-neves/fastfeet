64305e77141be1e377b36d0f522c239a1c1b73d3
Matheus Neves <matheus.neves@prodigious.com>
2020-02-04 14:05:11 -0300

    new routes/middleware

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
-import Router from "express";
+import Router from 'express';
+import RecipientController from './app/controllers/RecipientController';
+import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
-routes.get("/", (req, res) => {
-  res.json({ ok: true });
-});
+routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware);
+
+routes.post('/recipient', RecipientController.store);
+routes.put('/recipient/:id', RecipientController.update);
 
 export default routes;
